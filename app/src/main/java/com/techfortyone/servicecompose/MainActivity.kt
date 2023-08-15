daf292d9ad06bc21ec7d18fbfdd170ad841f7210
Saikrishna41 <saai.krrishna@gmail.com>
2023-08-15 18:21:30 +0800

    implemented service for notification

--- app/src/main/java/com/techfortyone/servicecompose/MainActivity.kt
+++ app/src/main/java/com/techfortyone/servicecompose/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.techfortyone.servicecompose
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
 import com.techfortyone.servicecompose.ui.theme.ServiceComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!hasForegroundServicePermissions() && !hasNotificationPermissions()) {
+            requestPermissions()
+
+        } else {
+            Toast.makeText(applicationContext, "permission granted",Toast.LENGTH_LONG).show()
+        }
         setContent {
             ServiceComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,10 +51,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Button(onClick = {
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.START.toString()
+                                startService(it)
+                            }
+                        }) {
+                            Text(text = "Start Service")
+                        }
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Button(onClick = {
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.STOP.toString()
+                                startService(it)
+                            }
+                        }) {
+                            Text(text = "Stop Service")
+                        }
+                    }
                 }
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun hasNotificationPermissions() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.POST_NOTIFICATIONS
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun hasForegroundServicePermissions() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.FOREGROUND_SERVICE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+        permissionsToRequest.add(Manifest.permission.POST_NOTIFICATIONS)
+        permissionsToRequest.add(Manifest.permission.FOREGROUND_SERVICE)
+        ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 101)
     }
 }
 
